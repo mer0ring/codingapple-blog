31479dc5dcbb2fe93d84f8222683a5ada4c9ac33
Lee Dohoon <mero.japan@gmail.com>
2022-07-22 17:43:02 +0900

    2022/07/22 codingapple blog update

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
   let [articleName, setArticleName] = useState(
     ['남자코트 추천', '강남 우동 맛집', '파이썬 독학']
   );
+  let [title, setTitle] = useState(0);
   let [like, setLike] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [userInput, setUserInput] = useState('');
 
   const articleNameChange = () => {
     let copyArticleName = [...articleName];
@@ -26,6 +28,12 @@ function App() {
     setModal(!modal);
   }
 
+  const arrayInput = () => {
+    let copyArticleName = [...articleName];
+    copyArticleName.unshift(userInput);
+    setArticleName(copyArticleName);
+  }
+
   return (
     <div className="App">
       <header className="black-nav">
@@ -38,9 +46,14 @@ function App() {
       {
         articleName.map((arrData, i) => {
           return (
-            <article className="list" onClick={modalStatusChange} key={i}>
+            <article 
+              className="list" 
+              onClick={() => {modalStatusChange(); setTitle(i);}} 
+              key={i}
+            >
               <h4>{arrData} 
-                <span onClick={() => {
+                <span onClick={(e) => {
+                  e.stopPropagation();
                   let copy = [...like];
                   copy[i] = copy[i] + 1;
                   setLike(copy);
@@ -48,34 +61,37 @@ function App() {
                 {like[i]}
               </h4>
               <p>2월 17일 발행</p>
+              <button onClick={() => {
+                let copy = [...articleName];
+                let copySlice = copy.slice(i, 1);
+                setArticleName(copySlice); // 여기 하다 말았음
+              }}>글삭제</button>
             </article>
           );
         })
       }
+
+      <input onChange={(e) => {
+        setUserInput(e.target.value);
+      }}/>
+      <button onClick={arrayInput}>글추가</button>
       
       {
-        modal ? <Modal articleName={articleName} /> : null
+        modal ? <Modal articleName={articleName} articleNameChange={articleNameChange} title={title} /> : null
       }      
 
     </div>
   );
 }
 
-/* 
-  1. props.articleName 으로 전달받은 글 제목을 제각각 모달에 표기
-  2. 글 수정 기능 완료시키기
-*/
-
 const Modal = (props) => {
   
-  const { articleName } = props.articleName;
-  
   return (
-    <div className="modal">
-      <h4>{}</h4>
+    <div className="modal">      
+      <h4>{props.articleName[props.title]}</h4>
       <p>date</p>
       <p>details</p>
-      <button>글수정</button>
+      <button onClick={props.articleNameChange}>글수정</button>
     </div>
   );
 }
